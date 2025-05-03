25ad9338a1d61b80fbdca0b3633c9446d37c263e
Metra Phirielle <88748759+Moouh2@users.noreply.github.com>
2025-05-03 23:24:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from notification_manager import NotificationManager
 from sms_adapter import SMSAdapter
-from newsletter import Newsletter, Subscriber
+from Newsletter import Newsletter, Subscriber
 
-# 1. Singleton (NotificationManager)
+# 1. Singleton - Gestionnaire central
 manager = NotificationManager.get_instance()
 
-# 2. Adapter (SMSAdapter)
+# 2. Adapter - Service SMS
 sms_adapter = SMSAdapter()
 
-# 3. Observer (Newsletter + Subscriber)
-newsletter = Newsletter()
-subscriber1 = Subscriber()
-newsletter.subscribe(subscriber1)
+# 3. Observer - Newsletter et Abonnés
+Newsletter = Newsletter()
 
-# Test 1 : Envoi via Singleton + Adapter
-manager.send_notification("Hello via SMS !", sms_adapter)
+# Boucle interactive
+while True:
+    print("\n=== Menu ===")
+    print("1. Envoyer une notification SMS")
+    print("2. S'abonner à la newsletter")
+    print("3. Envoyer une newsletter")
+    print("4. Quitter")
+    choix = input("Choix : ")
 
-# Test 2 : Notification via Observer
-newsletter.notify_all("Nouvelle promo disponible !")
+    if choix == "1":
+        message = input("Message SMS à envoyer : ")
+        manager.send_notification(message, sms_adapter)  # Singleton + Adapter
+
+    elif choix == "2":
+        user_name = input("Nom de l'utilisateur : ")
+        new_user = Subscriber(user_name)
+        Newsletter.subscribe(new_user)
+        print(f"{user_name} abonné(e) !")
+
+    elif choix == "3":
+        message = input("Message de la newsletter : ")
+        N1ewsletter.notify_all(message)  # Observer
+
+    elif choix == "4":
+        break
+
+    else:
+        print("Option invalide.")
